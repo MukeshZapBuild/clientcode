9ffed6f5567942764caaad45c093d3c1bfe1fccb
Mukesh <mukeshvaidya@zapbuild.com>
2019-10-10 14:09:02 +0530

    first-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { card } from "./../model/card";
+import { CARDS } from "./../../db-data";
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'namancode';
+  title = "namancode";
+  cards: card[] = CARDS;
 }
